48d482278bc8c55f0d6574c7c867c91fc3f28563
Ram Banstola <ram.banstola@live.com>
2019-06-29 14:56:27 +0300

    Customize footer

--- tests/Feature/AdminRouteTest.php
+++ tests/Feature/AdminRouteTest.php
@@ -2,20 +2,24 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class AdminRouteTest extends TestCase
 {
+    use WithoutMiddleware;
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testRootRequestRedirectedToAdmin()
     {
         $response = $this->get('/');
+        $response->assertStatus(302);
 
-        $response->assertStatus(200);
     }
+
 }
