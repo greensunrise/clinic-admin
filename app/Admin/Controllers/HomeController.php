60cc7f9e31631c36a85a349400f02fed59932d8d
Ram Banstola <ram.banstola@live.com>
2019-06-29 14:57:26 +0300

    Remove defaults from laravel admin

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -13,22 +13,6 @@ class HomeController extends Controller
     public function index(Content $content)
     {
         return $content
-            ->title('Dashboard')
-            ->description('Description...')
-            ->row(Dashboard::title())
-            ->row(function (Row $row) {
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
-                });
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
-                });
-            });
+            ->title(config('admin.name').' Dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('admin');
+    return redirect('/admin');
 });
